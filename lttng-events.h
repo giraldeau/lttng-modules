bc1fdeaad12c2365b4c604c5f1df5ccabc30b4c1
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-07-17 17:05:20 -0400

    Add kernel callstack context
     * v2: minor code cleanup
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- lttng-events.h
+++ lttng-events.h
@@ -164,6 +164,8 @@ struct lttng_ctx_field {
 		struct lttng_perf_counter_field *perf_counter;
 	} u;
 	void (*destroy)(struct lttng_ctx_field *field);
+	/* private data to keep state between get_size and record */
+	void *private;
 };
 
 struct lttng_ctx {
@@ -420,6 +422,7 @@ int lttng_add_vtid_to_ctx(struct lttng_ctx **ctx);
 int lttng_add_ppid_to_ctx(struct lttng_ctx **ctx);
 int lttng_add_vppid_to_ctx(struct lttng_ctx **ctx);
 int lttng_add_hostname_to_ctx(struct lttng_ctx **ctx);
+int lttng_add_callstack_to_ctx(struct lttng_ctx **ctx, int type);
 #if defined(CONFIG_PERF_EVENTS) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33))
 int lttng_add_perf_counter_to_ctx(uint32_t type,
 				  uint64_t config,
