bc29d6243d37b3773cdd2bfe4e608f7be824a022
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-07-17 17:05:20 -0400

    Add kernel callstack context
     * v2: minor code cleanup
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>
    
    Conflicts:
    	Makefile

--- lttng-abi.c
+++ lttng-abi.c
@@ -194,6 +194,9 @@ long lttng_abi_add_context(struct file *file,
 		return lttng_add_procname_to_ctx(ctx);
 	case LTTNG_KERNEL_CONTEXT_HOSTNAME:
 		return lttng_add_hostname_to_ctx(ctx);
+	case LTTNG_KERNEL_CONTEXT_CALLSTACK_KERNEL:
+	case LTTNG_KERNEL_CONTEXT_CALLSTACK_USER:
+		return lttng_add_callstack_to_ctx(ctx, context_param->ctx);
 	default:
 		return -EINVAL;
 	}
