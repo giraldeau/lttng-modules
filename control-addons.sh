735349f66b92571da83f4c4c8c1a3d6232f4b854
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-08-29 09:10:45 -0400

    Script to load/unload addons modules
    Three commands: load, unload and reload. For unload to work, there must be no
    tracing session running and lttng-sessiond must be stopped.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ control-addons.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+        echo "Usage : $0 [load|unload|reload]"
+        exit
+fi
+
+load_modules() {
+	echo "loading lttng-addons"
+	sudo modprobe lttng-modsign
+	sudo modprobe lttng-probe-addons
+	sudo modprobe lttng-addons
+	sudo modprobe lttng-syscall-entry
+	sudo modprobe lttng-mmap
+}
+
+unload_modules() {
+	echo "unloading lttng-addons"
+	sudo rmmod lttng-probe-addons
+	sudo rmmod lttng-addons
+	sudo rmmod lttng-syscall-entry
+	sudo rmmod lttng-mmap
+	sudo rmmod lttng-modsign
+}
+
+reload_modules() {
+	unload_modules
+	load_modules
+}
+
+case "$1" in
+load)
+	load_modules
+    ;;
+unload)
+	unload_modules
+    ;;
+reload)
+	reload_modules
+    ;;
+*) echo "unkown command $1"
+   ;;
+esac
+
