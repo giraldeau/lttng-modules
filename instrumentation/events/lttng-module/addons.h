d16a146e5758b7e0df75684b39de19093ac2ded8
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-07-17 14:56:10 -0400

    Use template for inet_sock events
    TCP event in/out are exactly the same, except for their name. Simplify the code
    by using template.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- instrumentation/events/lttng-module/addons.h
+++ instrumentation/events/lttng-module/addons.h
@@ -8,7 +8,7 @@
 #include <net/sock.h>
 #include <linux/tcp.h>
 
-TRACE_EVENT(inet_sock_local_in,
+DECLARE_EVENT_CLASS(inet_sock_local_template,
 	TP_PROTO(struct sock *sk, struct tcphdr *tcph),
 	TP_ARGS(sk, tcph),
 	TP_STRUCT__entry(
@@ -31,28 +31,13 @@ TRACE_EVENT(inet_sock_local_in,
 			__entry->check, __entry->window, __entry->flags)
 )
 
-TRACE_EVENT(inet_sock_local_out,
-	TP_PROTO(struct sock *sk, struct tcphdr *tcph),
-	TP_ARGS(sk, tcph),
-	TP_STRUCT__entry(
-		__field_hex(struct sock *, sk)
-		__field_network_hex(uint32_t, seq)
-		__field_network_hex(uint32_t, ack_seq)
-		__field_network_hex(uint16_t, check)
-		__field_network_hex(uint16_t, window)
-		__field_network_hex(uint16_t, flags)
-	),
-	TP_fast_assign(
-		tp_assign(sk, sk)
-		tp_assign(seq, tcph->seq)
-		tp_assign(ack_seq, tcph->ack_seq)
-		tp_assign(check, tcph->check)
-		tp_assign(window, tcph->window)
-		tp_assign(flags, *(((uint16_t *)tcph) + 6)) // flags are in network order
-	),
-	TP_printk("%p %x %x %x %x %x", __entry->sk, __entry->seq, __entry->ack_seq,
-			__entry->check, __entry->window, __entry->flags)
-)
+DEFINE_EVENT(inet_sock_local_template, inet_sock_local_in,
+		TP_PROTO(struct sock *sk, struct tcphdr *tcph),
+		TP_ARGS(sk, tcph))
+
+DEFINE_EVENT(inet_sock_local_template, inet_sock_local_out,
+		TP_PROTO(struct sock *sk, struct tcphdr *tcph),
+		TP_ARGS(sk, tcph))
 
 TRACE_EVENT(mmap_exec_file,
 		TP_PROTO(const char *name, unsigned long start, unsigned long len, unsigned long pgoff),
