cb672a5e7090b44d09f12b8cb30ef24b5fd777a3
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-09-05 22:40:08 -0400

    wip
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- lttng-context-callstack.c
+++ lttng-context-callstack.c
@@ -86,6 +86,7 @@ struct cs_def {
 	void (*save_func)(struct stack_trace *trace);
 	struct cs_set __percpu *items;
 	struct kref ref;
+	int active;
 };
 
 static struct cs_def cs_table[] = {
@@ -101,8 +102,84 @@ static struct cs_def cs_table[] = {
 	},
 	{ },
 };
-/* mutex to protect cs_def_table */
+
+/* mutex to protect cs_table */
 DEFINE_MUTEX(cs_table_mutex);
+
+static
+struct stack_trace *stack_trace_context(struct cs_def *def, int cpu)
+{
+	int nesting;
+	struct cs_set *set;
+	/*
+	 * get_cpu() is not required, preemption is already
+	 * disabled while event is written.
+	 *
+	 * max nesting is enforced in lib_ring_buffer_get_cpu().
+	 * Check it again as a safety net.
+	 */
+	set = per_cpu_ptr(def->items, cpu);
+	nesting = per_cpu(lib_ring_buffer_nesting, cpu) - 1;
+	if (nesting >= RING_BUFFER_MAX_NESTING) {
+		return NULL;
+	}
+	return &set->st[nesting];
+}
+
+/*
+ * In order to reserve the correct size, the callstack is computed. The
+ * resulting callstack is saved to be accessed in the record step.
+ */
+static
+size_t lttng_callstack_get_size(size_t offset, struct lttng_ctx_field *field,
+				struct lib_ring_buffer_ctx *ctx,
+				struct lttng_channel *chan)
+{
+	size_t size = 0;
+	struct stack_trace *trace;
+	struct cs_def *def = &cs_table[field->u.mode];
+
+	/* sequence length is mandatory */
+	size += lib_ring_buffer_align(offset, lttng_alignof(unsigned int));
+	size += sizeof(unsigned int);
+
+	/* do not write data if no space is available */
+	trace = stack_trace_context(def, ctx->cpu);
+	if (!trace)
+		return size;
+
+	/* reset stack trace, no need to clear memory */
+	trace->nr_entries = 0;
+
+	/* do the real work and reserve space */
+	def->save_func(trace);
+	size += lib_ring_buffer_align(offset, lttng_alignof(unsigned long));
+	size += sizeof(unsigned long) * trace->nr_entries;
+	return size;
+}
+
+static
+void lttng_callstack_record(struct lttng_ctx_field *field,
+			    struct lib_ring_buffer_ctx *ctx,
+			    struct lttng_channel *chan)
+{
+	int zero = 0;
+	struct cs_def *def = &cs_table[field->u.mode];
+	struct stack_trace *trace = stack_trace_context(def, ctx->cpu);
+
+	/* write length */
+	lib_ring_buffer_align_ctx(ctx, lttng_alignof(unsigned int));
+	if (!trace) {
+		chan->ops->event_write(ctx, &zero, sizeof(unsigned int));
+		return;
+	}
+	chan->ops->event_write(ctx, &trace->nr_entries, sizeof(unsigned int));
+
+	/* write entries */
+	lib_ring_buffer_align_ctx(ctx, lttng_alignof(unsigned long));
+	chan->ops->event_write(ctx, trace->entries,
+			sizeof(unsigned long) * trace->nr_entries);
+}
 
 /*
  * Initialize the callstack global definition. Increment reference count if the
@@ -116,18 +193,24 @@ int cs_data_init(struct cs_def *def)
 	struct stack_trace *item;
 
 	mutex_lock(&cs_table_mutex);
-	if (def->save_func) {
-		kref_get(&def->ref);
+	if (kref_get_unless_zero(&def->ref)) {
+		/* refcount >= 1 means the structure is already ready */
 		mutex_unlock(&cs_table_mutex);
 		return 0;
 	}
-	def->save_func = (void *) kallsyms_lookup_funcptr(def->save_func_name);
+
+	/* find the save_func */
 	if (!def->save_func) {
-		printk(KERN_WARNING "LTTng: symbol lookup failed: %s\n",
-				def->save_func_name);
-		mutex_unlock(&cs_table_mutex);
-		return -EINVAL;
+		def->save_func = (void *) kallsyms_lookup_funcptr(def->save_func_name);
+		if (!def->save_func) {
+			printk(KERN_WARNING "LTTng: symbol lookup failed: %s\n",
+					def->save_func_name);
+			mutex_unlock(&cs_table_mutex);
+			return -EINVAL;
+		}
 	}
+
+	/* alloc stack_trace data */
 	def->items = alloc_percpu(struct cs_set);
 	if (!def->items)
 		goto error_alloc_items;
@@ -143,6 +226,8 @@ int cs_data_init(struct cs_def *def)
 			item->max_entries = MAX_ENTRIES;
 		}
 	}
+
+	/* initialization completed */
 	kref_init(&def->ref);
 	mutex_unlock(&cs_table_mutex);
 	return 0;
@@ -174,215 +259,66 @@ void cs_data_release(struct kref *kref)
 		kfree(cs->entries_buf);
 	}
 	free_percpu(def->items);
-	def->save_func = NULL;
 	mutex_unlock(&cs_table_mutex);
 }
 
-//
-//static
-//struct stack_trace *stack_trace_context(struct lttng_ctx_field *field,
-//					struct lib_ring_buffer_ctx *ctx)
-//{
-//	int nesting;
-//	struct callstack_data *cs;
-//	/*
-//	 * get_cpu() is not required, preemption is already
-//	 * disabled while event is written.
-//	 *
-//	 * max nesting is checked in lib_ring_buffer_get_cpu().
-//	 * Check it again as a safety net.
-//	 */
-//	cs = per_cpu_ptr(callstacks[], ctx->cpu);
-//	nesting = per_cpu(lib_ring_buffer_nesting, ctx->cpu) - 1;
-//	if (nesting >= RING_BUFFER_MAX_NESTING) {
-//		return NULL;
-//	}
-//	return &cs->items[nesting];
-//}
-//
-///*
-// * In order to reserve the correct size, the callstack is computed. The
-// * resulting callstack is saved to be accessed in the record step.
-// */
-//static
-//size_t lttng_callstack_get_size(size_t offset, struct lttng_ctx_field *field,
-//				struct lib_ring_buffer_ctx *ctx,
-//				struct lttng_channel *chan)
-//{
-//	size_t size = 0;
-//	struct stack_trace *trace;
-//	struct field_data *fdata = field->private;
-//
-//	/* do not write data if no space is available */
-//	trace = stack_trace_context(field, ctx);
-//	if (!trace)
-//		return 0;
-//
-//	/* reset stack trace, no need to clear memory */
-//	trace->nr_entries = 0;
-//
-//	/* do the real work and reserve space */
-//	callstacks[fdata->mode].save_func(trace);
-//	size += lib_ring_buffer_align(offset, lttng_alignof(unsigned int));
-//	size += sizeof(unsigned int);
-//	size += lib_ring_buffer_align(offset, lttng_alignof(unsigned long));
-//	size += sizeof(unsigned long) * trace->nr_entries;
-//	return size;
-//}
-//
-//static
-//void lttng_callstack_record(struct lttng_ctx_field *field,
-//			    struct lib_ring_buffer_ctx *ctx,
-//			    struct lttng_channel *chan)
-//{
-//	struct stack_trace *trace = stack_trace_context(field, ctx);
-//
-//	if (!trace)
-//		return;
-//	lib_ring_buffer_align_ctx(ctx, lttng_alignof(unsigned int));
-//	chan->ops->event_write(ctx, &trace->nr_entries, sizeof(unsigned int));
-//	lib_ring_buffer_align_ctx(ctx, lttng_alignof(unsigned long));
-//	chan->ops->event_write(ctx, trace->entries,
-//			sizeof(unsigned long) * trace->nr_entries);
-//}
-//
-
-//
 static
 void lttng_callstack_destroy(struct lttng_ctx_field *field)
 {
 	struct cs_def *def = &cs_table[field->u.mode];
 	kref_put_mutex(&def->ref, cs_data_release, &cs_table_mutex);
 }
-//
-//static
-//int __lttng_add_callstack_generic(struct lttng_ctx **ctx, int mode)
-//{
-//	const char *ctx_name = cs_types[mode].name;
-//	struct lttng_ctx_field *field;
-//	int ret;
-//
-//	field = lttng_append_context(ctx);
-//	if (!field)
-//		return -ENOMEM;
-//	if (lttng_find_context(*ctx, ctx_name)) {
-//		printk("%s lttng_find_context failed\n", ctx_name);
-//		ret = -EEXIST;
-//		goto error_find;
-//	}
-//
-//	field->u.mode = mode;
-//	field->event_field.name = ctx_name;
-//	field->event_field.type.atype = atype_sequence;
-//	field->event_field.type.u.sequence.elem_type.atype = atype_integer;
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.size = sizeof(unsigned long) * CHAR_BIT;
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.alignment = lttng_alignof(long) * CHAR_BIT;
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.signedness = lttng_is_signed_type(unsigned long);
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.reverse_byte_order = 0;
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.base = 16;
-//	field->event_field.type.u.sequence.elem_type.u.basic.integer.encoding = lttng_encode_none;
-//
-//	field->event_field.type.u.sequence.length_type.atype = atype_integer;
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.size = sizeof(unsigned int) * CHAR_BIT;
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.alignment = lttng_alignof(unsigned int) * CHAR_BIT;
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.signedness = lttng_is_signed_type(unsigned int);
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.reverse_byte_order = 0;
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.base = 10;
-//	field->event_field.type.u.sequence.length_type.u.basic.integer.encoding = lttng_encode_none;
-//
-//	field->get_size_arg = lttng_callstack_get_size;
-//	field->record = lttng_callstack_record;
-//	field->destroy = lttng_callstack_destroy;
-//	wrapper_vmalloc_sync_all();
-//	return 0;
-//
-//error_find:
-//	lttng_remove_context_field(ctx, field);
-//	return ret;
-//}
-//
 
-//static
-//struct
-//{
-//	int cpu, i;
-//	struct stack_trace *item;
-//	struct callstack_data *cs;
-//	struct callstack_data __percpu *cs_set;
-//	struct field_data* fdata;
-//
-//	fdata = kzalloc(sizeof(unsigned long) * entries, GFP_KERNEL);
-//	if (!fdata)
-//		return NULL;
-//	cs_set = alloc_percpu(struct callstack_data);
-//	if (!cs_set)
-//		goto error_alloc;
-//
-//	fdata->data = cs_set;
-//	for_each_possible_cpu(cpu) {
-//		cs = per_cpu_ptr(cs_set, cpu);
-//		for (i = 0; i < RING_BUFFER_MAX_NESTING; i++) {
-//			item = &cs->items[i];
-//			item->entries = kzalloc(sizeof(unsigned long) * entries, GFP_KERNEL);
-//			if (!item->entries) {
-//				goto error_alloc;
-//			}
-//			item->max_entries = entries;
-//		}
-//	}
-//	fdata->mode = type;
-//	return fdata;
-//
-//error_alloc:
-//	field_data_free(fdata);
-//	return NULL;
-//}
-//
-//static
-//void lttng_callstack_data_free(struct field_data *fdata)
-//{
-//	int cpu, i;
-//	struct callstack_data *cs;
-//
-//	if (!fdata)
-//		return;
-//	for_each_possible_cpu(cpu) {
-//		cs = per_cpu_ptr(fdata->data, cpu);
-//		for (i = 0; i < RING_BUFFER_MAX_NESTING; i++) {
-//			kfree(cs->items[i].entries);
-//		}
-//	}
-//	free_percpu(fdata->data);
-//	kfree(fdata);
-//}
-//
-//static
-//int lttng_callstack_init(void)
-//{
-//	unsigned long func;
-//
-//	for (int mode = 0; mode < CALLSTACK_LAST; mode++) {
-//		struct cs_data *data = &csd[mode];
-//		struct cs_def *def = &cs_def_table[mode];
-//		data->def = def;
-//		func = kallsyms_lookup_funcptr(def->save_func_name);
-//		if (!func) {
-//			printk(KERN_WARNING "LTTng: symbol lookup failed: %s\n",
-//					def->save_func_name);
-//			continue;
-//		}
-//		data->save_func = (void *) func;
-//		data->items =
-//	}
-//	return 0;
-//}
-//EXPORT_SYMBOL_GPL(lttng_callstack_init);
-//
-//static
-//int lttng_callstack_end(void)
-//{
-//}
-//EXPORT_SYMBOL_GPL(lttng_callstack_end);
+static
+int __lttng_add_callstack_generic(struct lttng_ctx **ctx, int mode)
+{
+	struct cs_def *def = &cs_table[mode];
+	struct lttng_ctx_field *field;
+	int ret;
+
+	field = lttng_append_context(ctx);
+	if (!field)
+		return -ENOMEM;
+	if (lttng_find_context(*ctx, def->name)) {
+		printk("%s lttng_find_context failed\n", def->name);
+		ret = -EEXIST;
+		goto error_find;
+	}
+
+	ret = cs_data_init(def);
+	if (ret < 0)
+		goto error_find;
+
+	/* field declaration */
+	field->u.mode = mode;
+	field->event_field.name = def->name;
+	field->event_field.type.atype = atype_sequence;
+	field->event_field.type.u.sequence.elem_type.atype = atype_integer;
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.size = sizeof(unsigned long) * CHAR_BIT;
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.alignment = lttng_alignof(long) * CHAR_BIT;
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.signedness = lttng_is_signed_type(unsigned long);
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.reverse_byte_order = 0;
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.base = 16;
+	field->event_field.type.u.sequence.elem_type.u.basic.integer.encoding = lttng_encode_none;
+
+	field->event_field.type.u.sequence.length_type.atype = atype_integer;
+	field->event_field.type.u.sequence.length_type.u.basic.integer.size = sizeof(unsigned int) * CHAR_BIT;
+	field->event_field.type.u.sequence.length_type.u.basic.integer.alignment = lttng_alignof(unsigned int) * CHAR_BIT;
+	field->event_field.type.u.sequence.length_type.u.basic.integer.signedness = lttng_is_signed_type(unsigned int);
+	field->event_field.type.u.sequence.length_type.u.basic.integer.reverse_byte_order = 0;
+	field->event_field.type.u.sequence.length_type.u.basic.integer.base = 10;
+	field->event_field.type.u.sequence.length_type.u.basic.integer.encoding = lttng_encode_none;
+
+	field->get_size_arg = lttng_callstack_get_size;
+	field->record = lttng_callstack_record;
+	field->destroy = lttng_callstack_destroy;
+	wrapper_vmalloc_sync_all();
+	return 0;
+
+error_find:
+	lttng_remove_context_field(ctx, field);
+	return ret;
+}
 
 /**
  *	lttng_add_callstack_to_ctx - add callstack event context
@@ -402,13 +338,12 @@ int lttng_add_callstack_to_ctx(struct lttng_ctx **ctx, int type)
 {
 	switch (type) {
 	case LTTNG_KERNEL_CONTEXT_CALLSTACK_KERNEL:
-//		return __lttng_add_callstack_generic(ctx, CALLSTACK_KERNEL);
+		return __lttng_add_callstack_generic(ctx, CALLSTACK_KERNEL);
 	case LTTNG_KERNEL_CONTEXT_CALLSTACK_USER:
-//		return __lttng_add_callstack_generic(ctx, CALLSTACK_USER);
+		return __lttng_add_callstack_generic(ctx, CALLSTACK_USER);
 	default:
 		return -EINVAL;
 	}
-	cs_data_init(&cs_table[CALLSTACK_KERNEL]);
 }
 EXPORT_SYMBOL_GPL(lttng_add_callstack_to_ctx);
 
