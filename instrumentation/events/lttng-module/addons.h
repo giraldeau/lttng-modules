ff5344ad7dd0b6c87f5b9b4b115ff7b7c163ac00
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-11-26 11:29:48 -0500

    Add vmsync instrumentation
    The vmsync instrumentation aims at precise trace synchronization between the
    host and KVM guest virtual machines. Events in both directions is used by the
    convex hull synchronization algorithm.
    
    New events:
      * vmsync_gh_guest
      * vmsync_gh_host
      * vmsync_hg_guest
      * vmsync_hg_host
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- instrumentation/events/lttng-module/addons.h
+++ instrumentation/events/lttng-module/addons.h
@@ -51,6 +51,36 @@ LTTNG_TRACEPOINT_EVENT(sched_ttwu,
 	TP_printk("%d", __entry->tid)
 )
 
+LTTNG_TRACEPOINT_EVENT_CLASS(vmsync_template,
+	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
+	TP_ARGS(cnt, vm_uid),
+	TP_STRUCT__entry(
+		__field(unsigned int, cnt)
+		__field(unsigned long, vm_uid)
+	),
+	TP_fast_assign(
+		tp_assign(cnt, cnt)
+		tp_assign(vm_uid, vm_uid)
+	),
+	TP_printk("%u %lu", __entry->cnt, __entry->vm_uid)
+)
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(vmsync_template, vmsync_gh_guest,
+	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
+	TP_ARGS(cnt, vm_uid))
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(vmsync_template, vmsync_gh_host,
+	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
+	TP_ARGS(cnt, vm_uid))
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(vmsync_template, vmsync_hg_guest,
+	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
+	TP_ARGS(cnt, vm_uid))
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(vmsync_template, vmsync_hg_host,
+	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
+	TP_ARGS(cnt, vm_uid))
+
 #endif /* LTTNG_ADDONS_H_ */
 
 /* This part must be outside protection */
