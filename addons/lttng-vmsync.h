6f43399813525adb1f9ba57b4b37ca1e3563229a
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-11-26 11:29:48 -0500

    Add vmsync instrumentation
    The vmsync instrumentation aims at precise trace synchronization between the
    host and KVM guest virtual machines. Events in both directions is used by the
    convex hull synchronization algorithm.
    
    New events:
      * vmsync_gh_guest
      * vmsync_gh_host
      * vmsync_hg_guest
      * vmsync_hg_host
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ addons/lttng-vmsync.h
@@ -0,0 +1,14 @@
+/*
+ * lttng-vmsync.h
+ *
+ *  Created on: Jan 27, 2014
+ *      Author: francis
+ */
+
+#ifndef LTTNG_VMSYNC_H_
+#define LTTNG_VMSYNC_H_
+
+#define VMSYNC_INFO         KERN_INFO "VMSYNC: "
+#define VMSYNC_HYPERCALL_NR 5
+
+#endif /* LTTNG_VMSYNC_H_ */
