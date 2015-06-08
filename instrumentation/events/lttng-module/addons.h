624854b908b33af6d8a6d0762a6301cf491a707e
louispepin <louis.pepin.ce@gmail.com>
2015-06-08 11:39:24 -0400

    Add TP_FIELDS argument where necessary in addons.h

--- instrumentation/events/lttng-module/addons.h
+++ instrumentation/events/lttng-module/addons.h
@@ -11,24 +11,14 @@
 LTTNG_TRACEPOINT_EVENT_CLASS(inet_sock_local_template,
 	TP_PROTO(struct sock *sk, struct tcphdr *tcph),
 	TP_ARGS(sk, tcph),
-	TP_STRUCT__entry(
-		__field_hex(struct sock *, sk)
-		__field_network_hex(uint32_t, seq)
-		__field_network_hex(uint32_t, ack_seq)
-		__field_network_hex(uint16_t, check)
-		__field_network_hex(uint16_t, window)
-		__field_network_hex(uint16_t, flags)
-	),
-	TP_fast_assign(
-		tp_assign(sk, sk)
-		tp_assign(seq, tcph->seq)
-		tp_assign(ack_seq, tcph->ack_seq)
-		tp_assign(check, tcph->check)
-		tp_assign(window, tcph->window)
-		tp_assign(flags, *(((uint16_t *)tcph) + 6)) // flags are in network order
-	),
-	TP_printk("%p %x %x %x %x %x", __entry->sk, __entry->seq, __entry->ack_seq,
-			__entry->check, __entry->window, __entry->flags)
+	TP_FIELDS(
+		ctf_integer_hex(struct sock *, sk, sk)
+		ctf_integer_network_hex(uint32_t, seq, tcph->seq)
+		ctf_integer_network_hex(uint32_t, ack_seq, tcph->ack_seq)
+		ctf_integer_network_hex(uint16_t, check, tcph->check)
+		ctf_integer_network_hex(uint16_t, window, tcph->window)
+		ctf_integer_network_hex(uint16_t, flags, *(((uint16_t *)tcph) + 6))
+	)
 )
 
 LTTNG_TRACEPOINT_EVENT_INSTANCE(inet_sock_local_template, inet_sock_local_in,
@@ -42,27 +32,18 @@ LTTNG_TRACEPOINT_EVENT_INSTANCE(inet_sock_local_template, inet_sock_local_out,
 LTTNG_TRACEPOINT_EVENT(sched_ttwu,
 	TP_PROTO(int tid),
 	TP_ARGS(tid),
-	TP_STRUCT__entry(
-		__field(int, tid)
-	),
-	TP_fast_assign(
-		tp_assign(tid, tid)
-	),
-	TP_printk("%d", __entry->tid)
+	TP_FIELDS(
+		ctf_integer(int, tid, tid)
+	)
 )
 
 LTTNG_TRACEPOINT_EVENT_CLASS(vmsync_template,
 	TP_PROTO(unsigned int cnt, unsigned long vm_uid),
 	TP_ARGS(cnt, vm_uid),
-	TP_STRUCT__entry(
-		__field(unsigned int, cnt)
-		__field(unsigned long, vm_uid)
-	),
-	TP_fast_assign(
-		tp_assign(cnt, cnt)
-		tp_assign(vm_uid, vm_uid)
-	),
-	TP_printk("%u %lu", __entry->cnt, __entry->vm_uid)
+	TP_FIELDS(
+		ctf_integer(unsigned int, cnt, cnt)
+		ctf_integer(unsigned long, vm_uid, vm_uid)
+	)
 )
 
 LTTNG_TRACEPOINT_EVENT_INSTANCE(vmsync_template, vmsync_gh_guest,
