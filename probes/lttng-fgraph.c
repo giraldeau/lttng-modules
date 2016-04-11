07c1bb0eb46014808eb432ff2ad33b7aa567d580
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-04-11 11:30:04 -0400

    WIP: Register function graph hooks
    WARNING: This module's patch trigger kernel panic if loaded.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- probes/lttng-fgraph.c
+++ probes/lttng-fgraph.c
@@ -23,17 +23,33 @@
 
 #include <linux/module.h>
 #include <linux/ftrace.h>
+#include <linux/printk.h>
 #include <wrapper/kallsyms.h>
 #include <wrapper/tracepoint.h>
 #include <wrapper/vmalloc.h>
 #include <lttng-tracer.h>
 
-// "register_ftrace_graph"
-// "unregister_ftrace_graph"
-
 static int (*register_ftrace_graph_sym)(trace_func_graph_ret_t retfunc,
 			trace_func_graph_ent_t entryfunc);
 static void (*unregister_ftrace_graph_sym)(void);
+
+// called by prepare_ftrace_return()
+int lttng_fgraph_hook_entry(struct ftrace_graph_ent *ent)
+{
+	if (printk_ratelimit()) {
+		printk("lttng_fgraph_hook_return\n");
+	}
+
+	return 1;
+}
+
+// called by ftrace_return_to_handler()
+void lttng_fgraph_hook_return(struct ftrace_graph_ret *ret)
+{
+	if (printk_ratelimit()) {
+		printk("lttng_fgraph_hook_return\n");
+	}
+}
 
 static int __init lttng_fgraph_init(void)
 {
@@ -45,8 +61,17 @@ static int __init lttng_fgraph_init(void)
 	unregister_ftrace_graph_sym = (void *) kallsyms_lookup_funcptr("unregister_ftrace_graph");
 
 	printk("register %p unregister %p\n", register_ftrace_graph_sym, unregister_ftrace_graph_sym );
+	if (!register_ftrace_graph_sym || !unregister_ftrace_graph_sym) {
+		ret = -1;
+		goto out;
+	}
+
+	if (register_ftrace_graph_sym) {
+		ret = register_ftrace_graph_sym(lttng_fgraph_hook_return, lttng_fgraph_hook_entry);
+		printk("register fgraph hooks ret=%d\n", ret);
+	}
+
 	printk("lttng-fgraph loaded\n");
-	goto out;
 out:
 	return ret;
 }
@@ -54,6 +79,11 @@ module_init(lttng_fgraph_init);
 
 static void __exit lttng_fgraph_exit(void)
 {
+	if (unregister_ftrace_graph_sym) {
+		unregister_ftrace_graph_sym();
+		printk("unregister fgraph hooks\n");
+	}
+
 	printk("lttng-fgraph removed\n");
 }
 module_exit(lttng_fgraph_exit);
